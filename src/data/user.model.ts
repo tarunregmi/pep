449581d9c356e6f252033d58a78c2740471aaead
Tarun Regmi <tarunregmi.io@gmail.com>
2024-07-26 15:54:33 +0545

    add user model and data

---
+++ src/data/user.model.ts
@@ -0,0 +1,30 @@
+import { DataTypes, NOW } from 'sequelize';
+import sequelize from '../sequelize.js';
+
+const User = sequelize.define(
+  'user',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default User;
