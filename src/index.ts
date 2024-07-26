790ce84fbf4d03a3f6879f0c2f02496cd69919c8
Tarun Regmi <tarunregmi.io@gmail.com>
2024-07-26 15:56:15 +0545

    measure

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,13 @@ const insertUsers = async () => {
   try {
     await sequelize.sync({ force: true });
 
+    console.log('Start Inserting Data:');
+    console.time('insert_users');
+
     await User.bulkCreate(users, { validate: true });
+
+    console.timeEnd('insert_users');
+    console.log('End Inserting Data:');
   } catch (error) {
     console.error('Error inserting users:', error);
   } finally {
